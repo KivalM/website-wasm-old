2a04c22d408f8540be80408d43349520eb538beb
Kival Mahadew <kivalm@protonmail.com>
2022-03-24 12:58:12 +0200

    image

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,26 +1,46 @@
 use yew::prelude::*;
 
-pub struct Home;
+pub struct Home {
+    blurred: bool,
+}
 
-pub enum Msg {}
+pub enum Msg {
+    Clicked,
+}
 
 impl Component for Home {
     type Message = Msg;
     type Properties = ();
 
     fn create(ctx: &Context<Self>) -> Self {
-        Self
+        Self { blurred: false }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        html! {
-            <header>
-                <a href="#" class="header-lnk">
-                <div class="title">{"Kival Mahadew"}</div>
-                <button class="header-btn">{"see.more"}</button>
-                </a>
-            </header>
+        let onclick = ctx.link().callback(|_| Msg::Clicked);
 
+        let c = if self.blurred { "blur-bg" } else { "" };
+        let v = if self.blurred {
+            "hide header-lnk "
+        } else {
+            "header-lnk"
+        };
+        html! {
+            <header {onclick} class={c}>
+                <div class={v}   >
+                    <div class="title">{"Kival Mahadew"}</div>
+                    <button class="header-btn">{"see.more"}</button>
+                 </div>
+            </header>
         }
+    }
+
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::Clicked => {
+                self.blurred = !self.blurred;
+            }
+        }
+        true
     }
 }
